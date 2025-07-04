3dfd5f7f4928af8b28a6fc6b6b35c6a908c015a7
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 00:33:56 +0500

    Add src\data\players.js

---
+++ src\data\players.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    id: 1,
+    name: 'Babar Azam',
+    role: 'Captain',
+    image: 'https://example.com/babar-azam.jpg'
+  },
+  {
+    id: 2,
+    name: 'Mohammad Rizwan',
+    role: 'Wicket-keeper',
+    image: 'https://example.com/mohammad-rizwan.jpg'
+  },
+  // ...
+];
