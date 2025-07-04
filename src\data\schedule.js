3fc8933af08c79ef91ea289c0c3578c9987f4ebe
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 00:33:58 +0500

    Add src\data\schedule.js

---
+++ src\data\schedule.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    id: 1,
+    date: '2023-02-10',
+    opponent: 'Australia',
+    venue: 'Melbourne Cricket Ground'
+  },
+  {
+    id: 2,
+    date: '2023-02-15',
+    opponent: 'England',
+    venue: 'Lord\'s Cricket Ground'
+  },
+  // ...
+];
