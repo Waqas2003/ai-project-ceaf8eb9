10cb5b2f7c8829c9b3da55cf91e4a0cf258c3cb6
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 00:33:54 +0500

    Add src\data\news.js

---
+++ src\data\news.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    id: 1,
+    title: 'Pakistan Cricket Team Announces Squad for Upcoming Series',
+      },
+  {
+    id: 2,
+    title: 'Babar Azam Named Captain of Pakistan Cricket Team',
+      },
+  // ...
+];
